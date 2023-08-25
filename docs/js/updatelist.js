5892988c718572ecaf54895af87466a5deb5c251
Jordan-Tajheria <jordan.roberts@publicissapient.com>
2023-08-25 12:14:08 +0100

    Complete Level 11 code

---
+++ docs/js/updatelist.js
@@ -0,0 +1,21 @@
+<!-- make call to private backend API and retrieve invoices -->
+$.ajax({
+    type: "POST",
+    url: "../profile/post",
+    beforeSend: function(xhr){
+         xhr.setRequestHeader('X-API-ACCESS-Token', '29902913');
+    },    
+  })
+    .fail(function( jqXHR, textStatus ) {
+        console.error('Unable to retrieve backend data');
+        console.log(jqXHR);
+        console.log(textStatus);
+    })
+    .done(function( msg ) {
+        console.log(msg.invoices);
+        $.each(msg.invoices, function( index, value ) {
+            $('#invoices tbody').append('<tr><td>' + value.id + '</td><td>' + value.date + '</td><td>' + value.value +'</td></tr>');
+          console.log( value.id + ": " + value.value );
+        });
+    });
+  
